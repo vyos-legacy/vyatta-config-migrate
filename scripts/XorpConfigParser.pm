2551bb5623a05eb03e2e4517ed20783db8155865
Stephen Hemminger <stephen.hemminger@vyatta.com>
2010-05-06 09:18:55 -0700

    Fix XorpConfigParser package
    Need package statement on package.
    Add 1; at end so loading always exits with success

--- scripts/XorpConfigParser.pm
+++ scripts/XorpConfigParser.pm
@@ -2,9 +2,7 @@
 
 use lib "/opt/vyatta/share/perl5/";
 
-#package XorpConfigParser;
-
-use Data::Dumper;
+package XorpConfigParser;
 
 use strict;
 
@@ -587,7 +585,6 @@ sub parse {
             $i++;
         }
     }
-
-#    print Dumper($self);
 }
 
+1;
