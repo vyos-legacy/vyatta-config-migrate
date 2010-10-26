5104d3741582091684e12d224dd1c0c1dbbef477
Stephen Hemminger <stephen.hemminger@vyatta.com>
2010-10-25 17:09:01 -0700

    Revert "Allow passing file handle to parser"
    This reverts commit 37c4e49b27231b54e6640f1305631bfb1491aa95.

--- scripts/XorpConfigParser.pm
+++ scripts/XorpConfigParser.pm
@@ -437,28 +437,25 @@ sub output {
 # This method is used to parse the XORP config file specified into the internal tree
 # structure that the methods above process and manipulate.
 #
-#  $file	String of the filename to parse or a handle
+#  $file	String of the filename to parse
 #
 sub parse {
     my ( $self, $file ) = @_;
-    my $in;
 
     %data = ();
 
-    if (ref($file)) {
-	$in = $file;
-    } else {
-	open  $in, '<', $file
-	    or die "Error!  Unable to open file \"$file\".  $!";
-    }
+    open my $in, '<', $file
+      or die "Error!  Unable to open file \"$file\".  $!";
 
     my $contents = "";
     while (<$in>) {
         $contents .= $_;
     }
-    close $in if ($in != $file);
+    close $in;
 
     my @array_contents = split( '', $contents );
+
+    #	print scalar(@array_contents) . "\n";
 
     my $length_contents = @array_contents;
     my $colon           = 0;
